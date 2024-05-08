afbb3a933f6d63be7cf634eda564bc99c78049e9
GeofreyMkekajunior <gustavgeofrey739@gmail.com>
2024-05-08 14:36:16 +0300

    Create MKEKAJUNIOR.py

---
+++ MKEKAJUNIOR.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+import requests
+
+def get_weather(api_key, city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] == 200:
+        weather_data = {
+            "temperature": data["
